4a43c0debc96fb6e9a2031db20829eaff9356eeb
tomaszkrupa1 <tomaszkrupa1999@gmail.com>
2022-12-15 10:26:05 +0000

    Seeding files added

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,20 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
+require("dotenv").config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
 if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+  throw new Error("PGDATABASE not set");
 }
+
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        max: 2,
+      }
+    : {};
 
 module.exports = new Pool();
